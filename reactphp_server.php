b046aaa012dd14f685d9a83380f11074018cbf16
a.wasiak@sare.pl <a.wasiak@sare.pl>
2018-04-30 09:49:48 +0200

    reactphp - changed endpoint_mode=dnsrr

--- reactphp_server.php
+++ reactphp_server.php
@@ -12,15 +12,19 @@ require "vendor/autoload.php";
 
 $loop = React\EventLoop\Factory::create();
 
+$instanceId = getenv("INSTANCE_ID");
 $server = new StreamingServer([
     new LimitConcurrentRequestsMiddleware(20),
     new RequestBodyBufferMiddleware(2 * 1024 * 1024),
     new RequestBodyParserMiddleware(),
     new LimitConcurrentRequestsMiddleware(1),
-    function (ServerRequestInterface $request) {
-        return new Response(200, ['Content-Type' => 'text/plain'], "Hello World!\n");
+    function (ServerRequestInterface $request) use ($instanceId) {
+        file_put_contents("php://stdout", date("H:i:s") . " got request\n");
+        return new Response(200, ['Content-Type' => 'text/plain'], "Hello World! from $instanceId\n");
     }
 ]);
+
+file_put_contents("php://stdout", $instanceId . " starting server...\n");
 
 $socket = new React\Socket\Server("0.0.0.0:9000", $loop);
 $server->listen($socket);
